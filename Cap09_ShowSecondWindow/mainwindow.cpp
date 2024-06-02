e9f72fb3624edba4a38a8e700282d85f067e07cc
unknown <pedrovitorcomercial@gmail.com>
2024-06-02 20:21:39 -0300

    Lesson 9 - Blocking to move the first window

---
+++ Cap09_ShowSecondWindow/mainwindow.cpp
@@ -0,0 +1,38 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include "QApplication"
+#include "secdialog.h"
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+
+
+    SecDialog Secd;
+    Secd.setModal(true);
+    Secd.exec();
+
+    //secDialog = new SecDialog(this);
+    //secDialog->show();
+
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QApplication::quit();
+
+}
+
